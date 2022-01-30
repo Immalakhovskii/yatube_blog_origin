b4bcb63f00ace000381289c3ce474c8bdd4e6ad9
Sasha Malakhovskii <sky4place@gmail.com>
2022-01-30 12:02:21 +0300

    First Django templates added

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('Это самое начало Yatube')
+    template = 'posts/index.html'
+    return render(request, template)
 
-def group_posts(request, slug):
-    return HttpResponse(f'Страница группы {slug}')
+def group_posts(request, template):
+    template = 'posts/group_list.html'
+    return HttpResponse(request, template)
