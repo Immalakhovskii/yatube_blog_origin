b0442aa6f2469ebb2cf295530efeb64ae73513f3
Sasha Malakhovskii <sky4place@gmail.com>
2022-01-31 06:45:31 +0300

    Static and links added

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,19 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
     template = 'posts/index.html'
-    return render(request, template)
+    title = 'Yatube main'
+    context = {
+        'title': title,
+        'text': 'Это главная страница проекта Yatube',
+    }
+    return render(request, template, context)
 
-def group_posts(request, template):
+def group_posts(request, slug):
     template = 'posts/group_list.html'
-    return HttpResponse(request, template)
+    title = 'Yatube groups'
+    context = {
+        'title': title,
+        'text': 'Здесь будет информация о группах проекта Yatube',
+    }
+    return render(request, template, context, slug)
