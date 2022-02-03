a0560b75d6727971377405998345d43f38b2d993
Sasha Malakhovskii <sky4place@gmail.com>
2022-02-03 05:45:26 +0300

    Groups, admin page, links and proper styles added

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,19 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
+
 
 def index(request):
     template = 'posts/index.html'
-    title = 'Yatube main'
+    title = 'Yatube: Главная'
+    posts = Post.objects.order_by('-pub_date')[:10]
     context = {
         'title': title,
-        'text': 'Это главная страница проекта Yatube',
+        'posts': posts,
     }
     return render(request, template, context)
 
+
 def group_posts(request, slug):
     template = 'posts/group_list.html'
-    title = 'Yatube groups'
+    title = 'Yatube: Группы'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
         'title': title,
-        'text': 'Здесь будет информация о группах проекта Yatube',
+        'group': group,
+        'posts': posts,
     }
-    return render(request, template, context, slug)
+    return render(request, template, context)
