267897d0fe237847b6656e4e2b91a757d13d0d3b
Sasha Malakhovskii <sky4place@gmail.com>
2022-02-03 13:40:14 +0300

    Added some final corrections

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,10 +4,8 @@ from .models import Post, Group
 
 def index(request):
     template = 'posts/index.html'
-    title = 'Yatube: Главная'
     posts = Post.objects.order_by('-pub_date')[:10]
     context = {
-        'title': title,
         'posts': posts,
     }
     return render(request, template, context)
@@ -15,11 +13,9 @@ def index(request):
 
 def group_posts(request, slug):
     template = 'posts/group_list.html'
-    title = 'Yatube: Группы'
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
-        'title': title,
         'group': group,
         'posts': posts,
     }
